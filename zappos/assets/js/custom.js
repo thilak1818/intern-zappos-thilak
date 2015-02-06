111f26782bdc9c98ff714742295f4e4912a66c56
thilak1818 <thilak1818@gmail.com>
2015-02-06 16:07:28 -0600

    Profile-application-zapposIntern

---
+++ zappos/assets/js/custom.js
@@ -0,0 +1,93 @@
+(function($){
+
+	
+	$(document).ready(function() {
+
+		$('body').scrollspy({
+			target: '.navbar-custom',
+			offset: 50
+		})
+
+		$('#intro').backstretch([
+			'assets/images/bg3.jpg',
+			'assets/images/bg2.jpg'
+		], {duration: 3000, fade: 750});
+
+		var navbar = $('.navbar');
+		var navHeight = navbar.height();
+
+		$(window).scroll(function() {
+			if($(this).scrollTop() >= navHeight) {
+				navbar.addClass('navbar-color');
+			}
+			else {
+				navbar.removeClass('navbar-color');
+			}
+		});
+
+		if($(window).width() <= 767) {
+			navbar.addClass('custom-collapse');
+		}
+
+		$(window).resize(function() {
+			if($(this).width() <= 767) {
+				navbar.addClass('custom-collapse');
+			}
+			else {
+				navbar.removeClass('custom-collapse');
+			}
+		});
+
+		
+		wow = new WOW({
+			mobile: false
+		});
+		wow.init();
+
+
+		$('#portfolio1').magnificPopup({
+			delegate: 'a.pop-up',
+			type: 'image',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0,1]
+			},
+			image: {
+				titleSrc: 'title',
+				tError: 'The image could not be loaded.',
+			}
+		});
+			$('#portfolio2').magnificPopup({
+			delegate: 'a.pop-up',
+			type: 'image',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0,1]
+			},
+			image: {
+				titleSrc: 'title',
+				tError: 'The image could not be loaded.',
+			}
+		});
+			$(document).on('click','.navbar-collapse.in',function(e) {
+			if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+				$(this).collapse('hide');
+			}
+		});
+
+		$('a[href*=#]').bind("click", function(e){
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+		});
+
+		
+	
+
+	});
+
+})(jQuery);
